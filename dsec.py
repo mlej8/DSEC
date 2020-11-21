26072cdb53a950e2edb79cf6d594c75edbcc7390
Er Jun Li <er.li@mail.mcgill.ca>
2020-11-21 12:17:28 -0500

    Working on mnist

--- dsec.py
+++ dsec.py
@@ -1,7 +1,5 @@
 import numpy as np
 import torch
-from numpy import linalg as LA
-from scipy.spatial import distance
 
 """
 @authors: Michael Li and William Zhang
@@ -13,13 +11,15 @@ from scipy.spatial import distance
 ########################
 ### Helper functions ###
 ########################
-def compute_loss(D, ind_coef_matrix):
+
+def compute_loss(patterns, D, ind_coef_matrix, u, l):
     """ Objective function of DSEC """
     loss = 0 
     for key in D:
         if key in ind_coef_matrix:
             # implementation of equation (12): L(r_ij, f(x_i;w) dot f(x_j;w))
-            loss += torch.pow(torch.linalg.norm(torch.Tensor([D[key] - torch.dot(patterns[key[0]], patterns[key[1]])]), 2), 2)
+            estimated_similarity = torch.dot(patterns[key[0]], patterns[key[1]])
+            loss += torch.pow(torch.linalg.norm(torch.Tensor([D[key] - estimated_similarity]), 2), 2) + (u - l)
     return loss 
 
 def construct_dataset(patterns, u, l):
@@ -37,15 +37,16 @@ def construct_dataset(patterns, u, l):
 
 def labeled_pairwise_patterns_selection(indicator_feature1, indicator_feature2, u, l):
     """ Implementation of the pairwise labelling algorithm described in the paper. """
-    if similarity_estimation(indicator_feature1, indicator_feature2) > u:
+    similarity = similarity_estimation(indicator_feature1, indicator_feature2)
+    if similarity > u:
         return 1
-    elif similarity_estimation(indicator_feature1, indicator_feature2) <= l:
+    elif similarity <= l:
         return 0 
     else:
         return None # similarity between x_i and x_j is ambiguous, thus this pair will be omitted during training
 
 def similarity_estimation(indicator_feature1, indicator_feature2):
-    return distance.cosine(indicator_feature1, indicator_feature2)
+    return torch.nn.CosineSimilarity(dim=0)(indicator_feature1, indicator_feature2)
 
 def construct_indicator_coefficient_matrix(dataset):
     ind_coef_matrix = dict()
@@ -54,23 +55,78 @@ def construct_indicator_coefficient_matrix(dataset):
             ind_coef_matrix[key] = 1
     return ind_coef_matrix
 
-def dsec(patterns, dataloader, net):
+def cp_constraint(indicator_feature, p):
+    """ Implmentation of equation (11): c_p constraint """
+    # TODO: should I put this layer in network declaration or in algo with sequential?
+    # Find max value of indicator feature
+    maximum = torch.max(indicator_feature)
+
+    # Equation 11a: substract max value to all element of indicator feature and take the exponent 
+    intermediate = torch.exp(indicator_feature - maximum)
     
-    # initialize variables
-    net = 0 # TODO: randomly initialize network's weights
-    num_clusters = patterns.labels # k the number of clusters
+    #  equation 11b: divide every intermediate element by the p-norm of the intermediate feature vector
+    output = torch.div(intermediate, torch.linalg.norm(intermediate, ord=p))
+
+    return output
+
+def weights_init(layer):
+    """ initialize weights using normalized Gaussian initialization strategy """
+    # TODO implement this initialization strategy
+    if isinstance(layer, nn.Conv2d):
+        torch.nn.init.xavier_uniform_(layer.weight.data, gain=nn.init.calculate_gain('relu;))
+        if m.bias is not None:
+            torch.nn.init.zeros_(layer.bias.data)
+
+    if isinstance(layer, nn.Linear):
+        torch.nn.init.xavier_uniform_(layer.weight.data, gain=nn.init.calculate_gain('relu'))
+        if m.bias is not None:
+            torch.nn.init.zeros_(layer.bias.data)
+
+
+########################
+###  DSEC algorithm  ###
+########################
+
+def dsec(dataset, dnn):
+    """ Takes as input a PyTorch dataset and a DNN model """
+    
+    # initial variables
+    num_clusters = len(dataset.classes) 
     u = 0.95
     l = 0.05
-    lr = 0.01
+    batch_size = 32
+    p = 1
 
-    while not l > u:
+    # initialize weights using normalized Gaussian initialization strategy 
+    for layer in dnn:
+        weights_init(layer)
+    
+    # In DSEC, the devised constraint layer is always applied to DNNs 
+    model = torch.nn.Sequential(dnn, cp_constraint)
+
+    # load all the images
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,shuffle=True, num_workers=2)
+
+    # learning rate
+    lr = 0.001
+    # define optimizer
+    optimizer = torch.optim.RMSprop(lr=lr)
+
+    while u >= l:
+
+        # tracking total loss
+        total_loss = 0.0
+
         for batch in dataloader:
 
-            # QUESTION: should we get find all indicator features for each pattern here first? It would be something like a forward pass 
-            # output = net(batch)
+            # initialize indicator features (forward pass)
+            output = net(training_set)
 
-            # select training data from batch using formula (8), e.g. define labels for each indicator feature pair
-            D = construct_dataset(batch) # NOTE: DNN is shared 
+            # clear the parameter gradients
+            optimizer.zero_grad()
+
+            # construct pairwise similarities dataset, select training data from batch using formula (8), e.g. define labels for each indicator feature pair
+            D = construct_dataset(output) # NOTE: DNN is shared 
 
             # compute indicator coefficient matrix for batch
             ind_coef_matrix = construct_indicator_coefficient_matrix(D)
@@ -78,14 +134,37 @@ def dsec(patterns, dataloader, net):
             # QUESTION: update weights by minimizing (12) how?? optimizer.step()?
             loss = compute_loss(D, ind_coef_matrix)
 
-    # output clusters and use true label to report predicted labels to get ACC.
+            # accumulate loss
+            total_loss += loss
+
+            # backward pass to get all gradients
+            loss.backward()
+
+            # weights are updated on each batch that is gradually sampled from the original dataset
+            optimizer.step()
+
+        # update u and l: s(u,l) = u - l
+        u = u - lr
+        l = l + lr
+
+    # output clusters
     labels = []
-    for pattern in patterns:
-        ind_feature = net(pattern)
-        index = torch.max(ind_feature, 0)
+    for pattern in dataset.data:
+        
+        # clustering labels can be inferred via the learned indicator features purely, which are k-dimensional one-hot vectors ideally
+        indicator_feature = net(pattern)
+        
+        # patterns are clustered by locating the largest response of indicator feature
+        index = torch.argmax(indicator_feature)
+
         labels.append(index)
 
     return labels
 
-# QUESTION: how can we apply constraint at (3) to DNN ? 
-
+# TODO Implement the constraint function
+# TODO Implement the neural network for MNIST and CIFAR10
+# TODO Investigate the difference between using each batch to create indicator features or create at beginning.
+# TODO call evaluation funciton here ACC, NMI, etc.
+# TODO create a helper funciton to use true label to report predicted labels to get ACC.
+# TODO: optimizations of network's weights and u and l are alternating iterative performed. QUestion: What does this mean...
+# turn = 0
--- mnist.py
+++ mnist.py
@@ -1,35 +1,39 @@
 from __future__ import print_function
 
+from dsec import c_pp
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
 import torchvision
-import torchvision.transforms as transforms
+from utils import transform
 
-# The output of torchvision datasets are PILImage images of range [0, 1]. We transform them to Tensors and normalize range to [-1, 1]. 
-transform = transforms.Compose([transforms.ToTensor(), # transform to tensor
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # normalize range to [-1, 1]
-                                ])
-# 1. Load the MNIST training and test datasets using torchvision
+# Load the MNIST training and test datasets using torchvision
 trainset = torchvision.datasets.MNIST(root='./mnistdata', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=2)
 
 # Load test set using torchvision
 testset = torchvision.datasets.MNIST(root='./mnistdata', train=False,download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,shuffle=False, num_workers=2)
 
 ### NN
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        # TODO Gaussian noise layer
+        self.gn = 
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv3 = nn.Conv2d(32, 64, 3, 1)
+        self.pooling1 = nn.Conv2d(32, 64, 3, 1)
+        self.conv4 = nn.Conv2d(32, 64, 3, 1)
+        self.conv5 = nn.Conv2d(32, 64, 3, 1)
+        self.conv6 = nn.Conv2d(32, 64, 3, 1)
+        self.pooling2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv7 = nn.Conv2d(32, 64, 3, 1)
+        self.global_averaging_bn = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Conv2d(32, 64, 3, 1)
+        self.fc2 = nn.Conv2d(32, 64, 3, 1)
+        self.constraint_layer = c_p
 
     def forward(self, x):
         x = self.conv1(x)
@@ -45,5 +49,3 @@ class Net(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
-
-
