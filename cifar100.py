de6bb21681abdb1bb33d9fc75ae621241eaaa9b1
Michael Li <er.li@mail.mcgill.ca>
2020-11-28 22:02:41 -0500

    Training again...

--- cifar100.py
+++ cifar100.py
@@ -68,6 +68,5 @@ class Net(nn.Module):
         return output
 
 model_name= "cifar100"
-# model_path = dsec(trainset, Net(), model_name=model_name)
-# cluster(trainset, Net(), model_path, model_name=model_name)
-cluster(trainset, Net(), "models/cifar100-Nov-28-03-29-56.pth", model_name=model_name)
+model_path = dsec(trainset, Net(), model_name=model_name)
+cluster(trainset, Net(), model_path, model_name=model_name)
--- dsec_validate_job.sh
+++ dsec_validate_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1              # Number of GPU(s) per node
 #SBATCH --cpus-per-task=2         # CPU cores/threads
 #SBATCH --mem=16G                 # memory (per node)
-#SBATCH --time=00:10:00           # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+#SBATCH --time=10:00:00           # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
 #SBATCH --job-name=dsec         
 #SBATCH --output=logs/%x-validation-%j.out
 #SBATCH --mail-user=er.li@mail.mcgill.ca
